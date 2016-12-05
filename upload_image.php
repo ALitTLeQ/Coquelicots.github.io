fd9ef3ae5063dd2b2eefaedf66a05e9b79f8cd45
ALitTLeQ <ee803804ee@gmail.com>
2016-12-06 01:20:13 +0800

    add son

--- upload_image.php
+++ upload_image.php
@@ -4,7 +4,14 @@ $img = $_POST['imageUrl'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = $upload_dir . "image.png";
+$newfilename = rand(1,99999) . '.png');
+$file = $upload_dir . $newfilename;
 $success = file_put_contents($file, $data);
 print $success ? $file : 'Unable to save the file.';
+echo '
+{
+"title":"'.$newfilename.'",
+}'; 
+
+
 ?>
