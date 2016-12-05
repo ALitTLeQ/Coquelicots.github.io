ebe2fb67cc145cb52b90110f3bacb22e7fdfd583
ALitTLeQ <ee803804ee@gmail.com>
2016-12-06 01:25:26 +0800

    update

--- upload_image.php
+++ upload_image.php
@@ -5,6 +5,7 @@ $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
 $newfilename = rand(1,99999) . '.png');
+//$newfilename = 'image.png';
 $file = $upload_dir . $newfilename;
 $success = file_put_contents($file, $data);
 //print $success ? $file : 'Unable to save the file.';
