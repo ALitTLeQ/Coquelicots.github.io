f3aae493d284d7a4bf1917eb9913fe9a1a3faa11
ALitTLeQ <ee803804ee@gmail.com>
2016-12-12 20:06:01 +0800

    generate random value

--- upload_image.php
+++ upload_image.php
@@ -8,5 +8,16 @@ $newfilename = uniqid() . '.png';
 //$newfilename = 'image.png';
 $file = $upload_dir . $newfilename;
 $success = file_put_contents($file, $data);
-file_put_contents('filename.json', '{"title":"'.$newfilename.'"}');
+
+$content = array(
+  "title" => "https://alittleq.com/upload/".$newfilename,
+  "Protein" => rand(140,160),
+  "Sugar" => rand(0,2),
+  "SpecificGravity" => 1 + (rand(15, 24) / 1000),
+  "UrineOB" => rand(1,10) > 7,
+  "StoolOB" => rand(1,10) > 7,
+  "PH" => rand(35, 80) / 10,
+);
+
+file_put_contents('filename.json', $content);
 ?>
